dee060f6ddc54ed75a2264c14c72046c31b42033
Meke <donotmind@fake.com>
2023-05-08 17:51:03 +0200

    Index page working

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    process.env.ADDRESS
+    "0x91FF4BcdD7dCE6E7c5B041Df8c32bFDb70eB43AD"
 );
 
 export default instance;
