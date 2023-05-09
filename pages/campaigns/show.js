b923be45ac264c5cb89d4b2dce1538bb81d3227d
Meke <donotmind@fake.com>
2023-05-09 15:11:32 +0200

    Added show cards to display info

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
+import {Card} from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+
 class CampaignShow extends Component {
     constructor(props) {
         super(props);
@@ -20,10 +23,56 @@ class CampaignShow extends Component {
         };
     }
 
+    renderCards() {
+        const {
+            balance,
+            managerAddress,
+            minimumContribution,
+            requestsCount,
+            approversCount
+        } = this.props;
+
+        const items = [
+            {
+                header: managerAddress,
+                meta: 'Address of Manager',
+                description: 'The manager created this campaign and can create requests to withdraw money.',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: minimumContribution,
+                meta: 'Minimum Contribution (wei)',
+                description: 'You must contribute at least this much wei to become an approver.',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: requestsCount,
+                meta: 'Number of Requests',
+                description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: approversCount,
+                meta: 'Number of Approvers',
+                description: 'Number of people who have already donated to this campaign.',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: web3.utils.fromWei(balance, 'ether') + ' eth',
+                meta: 'Campaign Balance (ether)',
+                description: 'The balance is how much money this campaign has left to spend.',
+                style: {overflowWrap: 'break-word'}
+            },
+        ];
+
+        return <Card.Group items={items}/>;
+    }
+
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
         );
     }
