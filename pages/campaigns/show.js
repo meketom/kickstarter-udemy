8cead0de425146c37b9527c8019a3f1bd919f2f9
Meke <donotmind@fake.com>
2023-05-09 15:01:10 +0200

    Showing single campaigns

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
 import Layout from "../../components/Layout";
-
+import Campaign from "../../ethereum/campaign";
 class CampaignShow extends Component {
     constructor(props) {
         super(props);
     }
+
+    static async getInitialProps(props) {
+        const address = props.query.address;
+
+        const campaignDetails = await Campaign(address).methods.getSummary().call();
+
+        return {
+            minimumContribution: campaignDetails[0],
+            balance: campaignDetails[1],
+            requestsCount: campaignDetails[2],
+            approversCount: campaignDetails[3],
+            managerAddress: campaignDetails[4]
+        };
+    }
+
     render() {
         return (
             <Layout>
