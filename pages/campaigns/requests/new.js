abe39f0fe5eba2ebc9723bde34545480752df43f
Meke <donotmind@fake.com>
2023-05-09 18:34:31 +0200

    Added Request route

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Layout from "../../../components/Layout";
+
+class RequestNew extends Component {
+
+    render() {
+        return (
+            <Layout>
+                <h1>NEW REQUEST!</h1>
+            </Layout>
+        )
+    }
+}
+
+export default RequestNew;
--- routes.js
+++ routes.js
@@ -3,4 +3,6 @@ const routes = require('next-routes');
 module.exports = routes()
     .add('home', '/', 'index')
     .add('newCampaign', '/campaigns/new', 'campaigns/new')
-    .add('detailsCampaign', '/campaigns/:address', 'campaigns/show');
+    .add('detailsCampaign', '/campaigns/:address', 'campaigns/show')
+    .add('requestsCampaign', '/campaigns/:address/requests', 'campaigns/requests/index')
+    .add('addRequestsCampaign', '/campaigns/:address/requests/new', 'campaigns/requests/new');
